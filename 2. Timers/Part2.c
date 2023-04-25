c479b5a61ef8f57ddcee3548401c717a95abb2d4
Eddie Rosa <rosaed86@students.rowan.edu>
2023-04-25 15:31:34 -0400

    never committed this part. part 3 coming in soon

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -11,7 +11,7 @@
 
 void gpioInit();
 void timerInit();
-
+char LED = 0x00;
 void main(){
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
@@ -25,14 +25,33 @@ void main(){
 
     __bis_SR_register(LPM3_bits | GIE);
 
+    while(1)
+    {
+            if(LED==0x00)
+            {
+                TB1CTL = TBSSEL_1 | MC_2 | ID_2;
+            }
+            else if(LED==0x01)
+            {
+                TB1CTL = TBSSEL_1 | MC_2 | ID_1;
+            }
+            else if(LED==0x02)
+            {
+                TB1CTL = TBSSEL_1 | MC_2 | ID_0;
+            }
+
+        }
 }
 
 
 void gpioInit(){
     // @TODO Initialize the Red or Green LED
-    P1DIR &= ~BIT0;
+    P1DIR |= BIT0;
     // @TODO Initialize Button 2.3
     P2DIR &= ~BIT3;
+    P2IN &= ~BIT3;
+    P2REN |= BIT3;                                // Enables pull-up resistor on button P2.3
+
 
 }
 
@@ -56,6 +75,23 @@ void timerInit(){
 __interrupt void Port_2(void)
 {
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
+    P2IFG &= ~BIT3;
+
+    if ((P2IES &= BIT3) == BIT3)
+        {
+            switch (LED)
+            {
+                case 0x00:
+                    LED = 0x01;
+                    break;
+                case 0x01:
+                    LED = 0x02;
+                    break;
+                case 0x02:
+                    LED = 0x00;
+                    break;
+
+
 
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
 
@@ -63,10 +99,18 @@ __interrupt void Port_2(void)
 
 
 // Timer B1 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(TIMER1_B0_VECTOR))) Timer1_B0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
 {
     // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P1OUT ^= BIT0;
+    TB1CCR0 += 32768;
 }
 
 
